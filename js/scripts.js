de4645ecc46904147c1e2d16732ca9020d135093
aymnms <aymnms@users.noreply.github.com>
2025-07-19 09:00:10 +0000

    deploy: df36741399210595d3dcd97f06fa8f865c82b331

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    emailjs.init('vwDi-15NIctzAxQHK');
+
+    const form = document.getElementById('contactForm');
+    const submitBtn = form.querySelector('button[type="submit"]');
+    
+    function checkFormValidity() {
+        submitBtn.disabled = !form.checkValidity();
+    }
+
+    form.addEventListener('input', checkFormValidity);
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        if (!form.checkValidity()) {
+            form.reportValidity();
+            return;
+        }
+
+        emailjs.sendForm('service_xcsnvzp', 'template_hmgya9t', form)
+            .then(() => {
+                form.querySelector('#submitSuccessMessage').classList.remove('d-none');
+                form.reset();
+                checkFormValidity();
+            }, (error) => {
+                form.querySelector('#submitErrorMessage').classList.remove('d-none');
+            });
+    });
+
+    checkFormValidity();
+});
